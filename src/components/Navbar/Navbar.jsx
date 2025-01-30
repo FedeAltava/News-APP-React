50d2fba316482818ab930232b27687f65935a8b7
Federico <fedealtava1992@gmail.com>
2025-01-30 12:54:14 +0100

    active route color navBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import './Navbar.css'
 import { NavLink } from 'react-router-dom'
 const NavBar =()=>{
+    const activeStyle = {color: 'red'}
     return(
         <nav className="nav">
             <ul>
-                <li><NavLink to ="/">Home</NavLink></li>
-                <li><NavLink to ="Category">Category</NavLink></li>
+                <li><NavLink to ="/" style={({isActive})=>isActive ? activeStyle : undefined}>Home</NavLink></li>
+                <li><NavLink to ="Category" style={({isActive})=>isActive ? activeStyle : undefined}>Category</NavLink></li>
             </ul>
         </nav>
     )
