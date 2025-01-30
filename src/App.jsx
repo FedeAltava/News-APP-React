8dc3020a0a1c9caa81bea54425f6c07b70844f23
Federico <fedealtava1992@gmail.com>
2025-01-30 11:52:08 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@
 import './App.css'
 import Category from './pages/Category/Category'
 import Home from './pages/Home/Home'
-
+import NewsDetail from './pages/NewsDetail/NewsDetail'
 function App() {
   
   return (
     <>
      <Home />
      <Category />
+     <NewsDetail />
     </>
   )
 }
---
+++ src/pages/NewsDetail/NewsDetail.jsx
@@ -0,0 +1,11 @@
+const NewsDetail = ()=>{
+
+    return(
+        <>
+        
+        </>
+    )
+
+}
+
+export default NewsDetail
